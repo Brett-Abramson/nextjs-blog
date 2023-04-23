a069425e35778c43ea15c2f994448eccc8ea8bdf
Brett Abramson <brett.m.abramson@gmail.com>
2023-04-23 13:06:19 -0500

    polished dynamic pages and added Links to homepage

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,9 +1,12 @@
 import Layout from "../../components/layout";
 import { getAllPostIds, getPostData } from "../../lib/posts";
+import Head from "next/head";
+import Date from "../../components/date"
+import utilStyles from '../../styles/utils.module.css';
 
 
 const getStaticProps = async ( {params} ) => {
-    const postData = getPostData(params.id);
+    const postData = await getPostData(params.id);
     return {
         props: {
             postData,
@@ -19,15 +22,20 @@ const getStaticPaths = async () => {
     };
 }
 
-const Post = ( {postData }) => {
+const Post = ( { postData }) => {
 
     return (
         <Layout>
-            {postData.title}
-            <br />
-            {postData.id}
-            <br />
-            {postData.date}
+            <Head>
+                <title>{postData.title}</title>
+            </Head>
+            <article>
+                <h1 className={utilStyles.headingXL}>{postData.title}</h1>
+                <div className={utilStyles.lightText}>
+                    <Date dateString={postData.date} />
+                </div>
+                <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+            </article>
         </Layout>
     )
 }
